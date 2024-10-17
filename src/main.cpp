6343df655384815179bdc3c2392cee1c9b75c4e9
unknown <thomaskuwaikwan@gmail.com>
2024-10-17 12:44:20 +0800

    created third blinking speed-mMade the changing pattern more chaotic

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,88 @@
 #include <Arduino.h>
 
+//set up the blinking speed (in ms)
 int quickDelay = 200 ;
+int intermediateDelay = 500;
 int slowDelay = 1000 ;
 
+//variable to control switching of tasks
+int count = 0;
+
+//create corresponding task handles for suspend and resume
 TaskHandle_t slow;
+TaskHandle_t intermediate;
 TaskHandle_t quick;
 
+// Normal blinking sequence: quick->intermediate->slow->quick...
+// Abnomral blinking sequence(count is multiple of 3): slow->intermediate->quick->slow...
 void q_Blink(void* parameter)
 {
-  for(int count1 = 0;;)
+  for(;;)
   {
     digitalWrite(LED_BUILTIN, 1);
     vTaskDelay(quickDelay / portTICK_PERIOD_MS);
     digitalWrite(LED_BUILTIN,0);
     vTaskDelay(quickDelay / portTICK_PERIOD_MS);  
-    count1++;
-    if (count1 %2) 
+    count++;
+    if (count %3) //abnormal
+    {
+      vTaskResume(slow);
+      vTaskSuspend(intermediate);
+    }
+    else //normal
+    {
+      vTaskResume(intermediate);
+      vTaskSuspend(slow);
+    }
+    vTaskSuspend(NULL); //suspend current task
+  }
+  
+}
+
+void i_Blink(void* parameter)
+{
+  for(;;)
+  {
+    digitalWrite(LED_BUILTIN, 1);
+    vTaskDelay(intermediateDelay / portTICK_PERIOD_MS);
+    digitalWrite(LED_BUILTIN,0);
+    vTaskDelay(intermediateDelay / portTICK_PERIOD_MS);  
+    count++;
+    if (count %3) //abnomral
     {
       vTaskResume(slow);
       vTaskSuspend(quick);
     }
+    else //normal
+    {
+      vTaskResume(quick);
+      vTaskSuspend(slow);
+    }
+    vTaskSuspend(NULL); //suspend current task
   }
   
 }
 
 void s_Blink(void* parameter)
 {
-  for (int count2 = 0; ;)
+  for (;;)
   {
     digitalWrite(LED_BUILTIN, 1);
     vTaskDelay(slowDelay / portTICK_PERIOD_MS);
     digitalWrite(LED_BUILTIN,0);
     vTaskDelay(slowDelay / portTICK_PERIOD_MS); 
-    count2++;
-    if (count2 %2) 
+    count++;
+    if (count % 3) //abnoraml
     {
-      vTaskResume(quick);
-      vTaskSuspend(slow);
+      vTaskResume(intermediate);
+      vTaskSuspend(quick);
     }
+    else{ //normal
+      vTaskResume(quick);
+      vTaskSuspend(intermediate);
+    }
+      
+    vTaskSuspend(NULL); //suspend current task
   }
    
 }
@@ -47,6 +92,7 @@ void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
   xTaskCreatePinnedToCore(q_Blink, "Quick_Blink", 1024, NULL, 1, &quick, 1);
+  xTaskCreatePinnedToCore(i_Blink, "Intermediate_Blink", 1024, NULL, 1, &intermediate, 1);
   xTaskCreatePinnedToCore(s_Blink, "Slow_Blink", 1024, NULL, 1, &slow, 1);
 }
 
